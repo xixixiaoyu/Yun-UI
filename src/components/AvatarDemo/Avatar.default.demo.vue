eef00708aec87ba0dc21f55ef61d835814861767
yunmu <1416900346@qq.com>
2025-06-16 22:51:25 +0800

    fix(AvatarDemo): 更新默认头像图片链接

--- src/components/AvatarDemo/Avatar.default.demo.vue
+++ src/components/AvatarDemo/Avatar.default.demo.vue
@@ -3,7 +3,7 @@
 </demo>
 
 <template>
-  <Avatar src="//tva2.sinaimg.cn/mw690/007c1Ltfgy1h4a6gg3r12j30u00u0ta3.jpg" alt="avatar" />
+  <Avatar src="https://q1.qlogo.cn/g?b=qq&s=100&nk=1057072668" alt="avatar" />
 
   <Avatar src="https://q1.qlogo.cn/g?b=qq&s=100&nk=1057072668" alt="avatar" round />
 </template>
